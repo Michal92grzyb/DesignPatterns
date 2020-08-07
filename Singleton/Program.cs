427c5a15004f1838b24a5d97d939ebf3412dda57
michal92grzyb <michal92grzyb@gmail.com>
2020-08-07 19:44:38 +0200

    Singeton added

---
+++ Singleton/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class President
+    {
+        static President instance;
+        // Private constructor
+        private President()
+        {
+            //Hiding the Constructor
+        }
+
+        // Public constructor
+        public static President GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new President();
+            }
+            return instance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            President a = President.GetInstance();
+            President b = President.GetInstance();
+
+            Console.WriteLine((a == b).ToString());
+        }
+    }
+}
