0d17c16f2593ff42926b772c469be952df447413
michal92grzyb <michal92grzyb@gmail.com>
2020-08-14 17:40:21 +0200

    Adapter added

---
+++ Adapter/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Adapter
+{
+    // when you want to wrap object that is otherwise incompatible with some interface or abstract class, you can use Adapter pattern to do it
+    // instead of changing the code.
+    interface ILion
+    {
+        void Roar();
+    }
+
+    class AfricanLion : ILion
+    {
+        public void Roar()
+        {
+
+        }
+    }
+
+    class AsiaLion : ILion
+    {
+        public void Roar()
+        {
+
+        }
+    }
+
+    // This needs to be added to the game
+    class WildDog
+    {
+        public void Bark()
+        {
+        }
+    }
+
+    // Adapter around wild dog to make it compatible with our game
+    class WildDogAdapter : ILion
+    {
+        private WildDog mDog;
+        public WildDogAdapter(WildDog dog)
+        {
+            this.mDog = dog;
+        }
+
+        // the place where WildDog is being adapted to match ILion interface
+        public void Roar()
+        {
+            mDog.Bark();
+        }
+    }
+
+    class Hunter
+    {
+        public void Hunt(ILion lion)
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wildDog = new WildDog();
+            var wildDogAdapter = new WildDogAdapter(wildDog);
+
+            var hunter = new Hunter();
+            hunter.Hunt(wildDogAdapter);
+
+            Console.ReadLine();
+        }
+    }
+}
